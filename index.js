0d31bdfd7fd4ba568b97a1732ce4edd4373ef278
vladka-kl <84926603+vladka-kl@users.noreply.github.com>
2022-11-07 12:15:04 +0100

    issue solved

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-requestAnimationFrame("dotenv").config();
+require("dotenv").config();
 
-cosnole.log(
+console.log(
   `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}.`
 );
